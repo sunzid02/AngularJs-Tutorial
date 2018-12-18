392b4f4a229fdcf275669b31999dea643b2f60a3
Sunzidmahmud <sunzid.sarker@cloudwell.co>
2018-12-18 13:38:54 +0600

    Handling events of button

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -41,4 +41,22 @@ var myApp = angular
                 ];
 
                 $scope.menz = menz;
+              })
+              .controller("HandlingEventsController", function ($scope) {
+                var technologies = [
+                  { name: "C#", likes:0, dislikes: 0},
+                  { name: "ASP.NET", likes:0, dislikes: 0},
+                  { name: "SQL Server", likes:0, dislikes: 0},
+                  { name: "AngularJS", likes:0, dislikes: 0}
+                ];
+
+                $scope.technologies = technologies;
+
+                $scope.incrementLikes = function (technology) {
+                  technology.likes++;
+                }
+
+                $scope.incrementDisLikes = function (technology) {
+                  technology.dislikes++;
+                }
               });
