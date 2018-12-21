96161581f9df0aed542ee213d904ee8352248137
sunzid02 <sunzid02@gmail.com>
2018-12-21 22:21:01 +0600

    search filter in a table

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -84,6 +84,7 @@ var myApp = angular
                 $scope.employees = employees;
                 $scope.sortColumn = "name";
               })
+
               .controller("sortRowsByTableHeaderController", function ($scope) {
                 var employees = [
                     { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000 },
@@ -104,16 +105,16 @@ var myApp = angular
                   after click that column it will change to desc order
                 */
                 $scope.sortData = function (column) {
-                    // if ($scope.sortColumn == column)
-                    // {
-                    //   $scope.reverseSort = !$scope.reverseSort; //true or false..
-                    // }
-                    // else
-                    // {
-                    //   $scope.reverseSort = false;
-                    // }
+                    if ($scope.sortColumn == column)
+                    {
+                      $scope.reverseSort = !$scope.reverseSort; //true or false..
+                    }
+                    else
+                    {
+                      $scope.reverseSort = false;
+                    }
                     // or
-                    $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+                    // $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
                     $scope.sortColumn = column;
                 }
 
@@ -128,4 +129,16 @@ var myApp = angular
                     return '';
                 }
 
+              })
+
+              .controller("SearchController", function ($scope) {
+                var employees = [
+                    { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000 },
+                    { name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender: "Female", salary: 68000 },
+                    { name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", salary: 57000 },
+                    { name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender: "Female", salary: 53000 },
+                    { name: "Todd", dateOfBirth: new Date("December 30, 1983"), gender: "Male", salary: 60000 }
+                ];
+
+                $scope.employees = employees;
               });
