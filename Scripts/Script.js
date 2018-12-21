369bf153669b263f8169b2ffb745ec3a03b8c0e1
sunzid02 <sunzid02@gmail.com>
2018-12-21 21:24:52 +0600

    sort rows by clicking on table header

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -83,4 +83,49 @@ var myApp = angular
 
                 $scope.employees = employees;
                 $scope.sortColumn = "name";
+              })
+              .controller("sortRowsByTableHeaderController", function ($scope) {
+                var employees = [
+                    { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000 },
+                    { name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender: "Female", salary: 68000 },
+                    { name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", salary: 57000 },
+                    { name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender: "Female", salary: 53000 },
+                    { name: "Todd", dateOfBirth: new Date("December 30, 1983"), gender: "Male", salary: 60000 }
+                ];
+
+                $scope.employees = employees;
+                $scope.sortColumn = "name";
+                $scope.reverseSort = false;
+
+
+                /*
+                  This function will provide the sort capability,
+                  it works like if your column adjust with ASC
+                  after click that column it will change to desc order
+                */
+                $scope.sortData = function (column) {
+                    // if ($scope.sortColumn == column)
+                    // {
+                    //   $scope.reverseSort = !$scope.reverseSort; //true or false..
+                    // }
+                    // else
+                    // {
+                    //   $scope.reverseSort = false;
+                    // }
+                    // or
+                    $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+                    $scope.sortColumn = column;
+                }
+
+                $scope.getSortClass = function (column) {
+
+                    if ($scope.sortColumn == column) {
+                        return $scope.reverseSort // if the value is true we are going to set the data to desc order
+                          ? 'arrow-down'
+                          : 'arrow-up';
+                    }
+
+                    return '';
+                }
+
               });
